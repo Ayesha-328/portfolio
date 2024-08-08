351f6241337319b444c4c9357f30c2c9e4ea0ea6
Ayesha Najeeb <ayeshanajeeb2803@gmail.com>
2024-08-08 14:56:54 +0500

    updated 8/8/2024

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react'
-import {Link } from "react-scroll"
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from "react-scroll";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Nav() {
-    const [navOpen, setNavOpen] = useState(false)
-    console.log(navOpen)
+    const [navOpen, setNavOpen] = useState(false);
+    const ref = useRef();
 
     function toggle() {
-        setNavOpen(prevNavOpen => !prevNavOpen)
+        setNavOpen(prevNavOpen => !prevNavOpen);
     }
-
-    const ref = useRef();
 
     useEffect(() => {
         const handler = (event) => {
@@ -20,15 +17,13 @@ function Nav() {
                 setNavOpen(false);
             }
         };
-        document.addEventListener("mousedown", handler)
-        // CleanUp function
-        return (() => {
-            document.removeEventListener("mouseover", handler)
-        })
-    }, [navOpen])
+        document.addEventListener("mousedown", handler);
+        return () => {
+            document.removeEventListener("mousedown", handler);
+        };
+    }, [navOpen]);
 
     return (
-        // When you apply links please see the toggle btn thing 
         <nav ref={ref} className="navbar">
             <div className="nav-container">
                 <div className="site-logo">
@@ -36,70 +31,66 @@ function Nav() {
                     <h2 className="sub-logo">Najeeb</h2>
                 </div>
 
-
-                <ul className={`${navOpen ? "display-menu" : ""} menu-items`} >
-                    <li className="list-item"><Link 
-      activeClass="active" 
-      to="hero" 
-      spy={true} 
-      offset={-100}
-      smooth={true} 
-      duration={500} 
-      onSetActive={toggle}
-    >Home</Link></li>
-                    <li className="list-item"><Link 
-      activeClass="active" 
-      to="about" 
-      spy={true} 
-      smooth={true} 
-      duration={500} 
-      onSetActive={toggle}
-    >About</Link></li>
-                    <li className="list-item"><Link 
-      activeClass="active" 
-      to="projects" 
-      spy={true} 
-      smooth={true} 
-      offset={-100} 
-      duration={500} 
-      onSetActive={toggle}
-    >Projects</Link></li>
-                    <li className="list-item"><Link 
-      activeClass="active" 
-      to="skills" 
-      spy={true} 
-      smooth={true} 
- 
-      duration={500} 
-      onSetActive={toggle}
-    >Skills</Link></li>
-                    <li className="list-item"><Link 
-      activeClass="active" 
-      to="certificates" 
-      spy={true} 
-      smooth={true} 
- 
-      duration={500} 
-      onSetActive={toggle}
-    >Certificates</Link></li>
+                <ul className={`${navOpen ? "display-menu" : ""} menu-items`}>
+                    <li className="list-item"><Link
+                        activeClass="active"
+                        to="hero"
+                        spy={true}
+                        offset={-100}
+                        smooth={true}
+                        duration={500}
+                        onClick={() => setNavOpen(false)} // Close the menu on click
+                    >Home</Link></li>
+                    <li className="list-item"><Link
+                        activeClass="active"
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        onClick={() => setNavOpen(false)}
+                    >About</Link></li>
+                    <li className="list-item"><Link
+                        activeClass="active"
+                        to="projects"
+                        spy={true}
+                        smooth={true}
+                        offset={-100}
+                        duration={500}
+                        onClick={() => setNavOpen(false)}
+                    >Projects</Link></li>
+                    <li className="list-item"><Link
+                        activeClass="active"
+                        to="skills"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        onClick={() => setNavOpen(false)}
+                    >Skills</Link></li>
+                    <li className="list-item"><Link
+                        activeClass="active"
+                        to="certificates"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        onClick={() => setNavOpen(false)}
+                    >Certificates</Link></li>
                 </ul>
 
                 <div className="side-menu">
-                <Link  
-      to="contact" 
-      spy={true} 
-      smooth={true} 
-      duration={500} 
-      onSetActive={toggle}
-    ><button className="btn contact-btn">Contact Me</button></Link>
+                    <Link
+                        to="contact"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        onClick={() => setNavOpen(false)}
+                    ><button className="btn contact-btn">Contact Me</button></Link>
                     <div onClick={toggle} className="toggle-btn">
                         <FontAwesomeIcon icon={faBars} />
                     </div>
-
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
-export default Nav
+export default Nav;
